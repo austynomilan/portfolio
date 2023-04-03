b51184b2224cb443797d4efde1eea61acc374df4
Augustine Odoh O <62001398+austynomilan@users.noreply.github.com>
2023-04-03 13:23:47 +0100

    layout fully developed

--- austinePortfolio/src/Components/Layout/Index.jsx
+++ austinePortfolio/src/Components/Layout/Index.jsx
@@ -1,11 +1,19 @@
 import "./index.scss"
 import Sidebar from "../Sidebar"
+import { Outlet } from "react-router-dom"
 
 
 const Layout = () => {
     return(
-        <div>
+        <div className="App">
             <Sidebar />
+            <div className="page">
+                <span className="tags top-tags">&lt;body&gt;</span>
+                <Outlet />
+                <span className="tags bottom-tags"> &lt;/body&gt;</span>
+                <br />
+                <span className=" tags bottom-tags-html">&lt;/html&gt;</span>
+            </div>
         </div>
     )
 }
--- austinePortfolio/src/Components/Sidebar/index.jsx
+++ austinePortfolio/src/Components/Sidebar/index.jsx
@@ -1,6 +1,7 @@
 import './index.scss'
 import { Link, NavLink } from 'react-router-dom'
-import LogoS from '../../assets/images/logo-s.png'
+import LogoA from '../../assets/images/A.png'
+import { FaGithub, FaLinkedinIn, FaTwitter } from 'react-icons/fa'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHome, faUser, faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import LogoSub from '../../assets/images/logo_sub.png'
@@ -9,21 +10,59 @@ const Sidebar = () => {
   return (
     <div className="w-24 h-screen bg-zinc-900 justify-center py-6">
       <Link to="/">
-        <img src={LogoS} alt="logo" className="w-12 h-auto m-auto" />
+        <img src={LogoA} alt="logo" className="w-20 h-auto m-auto" />
         <img src={LogoSub} alt="ausine" className="w-18 h-auto py-2" />
       </Link>
-<nav>
-   <NavLink className='active' exact="true" to="/">
-        <FontAwesomeIcon  icon={faHome} />
-      </NavLink>
-      <NavLink  activeclassname='active' className='about-link' exact="true" to="/about">
-        <FontAwesomeIcon icon={faUser} />
-      </NavLink>
-      <NavLink  activeclassname='active' className='contact-link' exact="true" to="/contact">
-        <FontAwesomeIcon icon={faEnvelope} />
-      </NavLink> 
-</nav>
-      
+      <nav>
+        <NavLink className="active" exact="true" to="/">
+          <FontAwesomeIcon icon={faHome} />
+        </NavLink>
+        <NavLink
+          activeclassname="active"
+          className="about-link"
+          exact="true"
+          to="/about"
+        >
+          <FontAwesomeIcon icon={faUser} />
+        </NavLink>
+        <NavLink
+          activeclassname="active"
+          className="contact-link"
+          exact="true"
+          to="/contact"
+        >
+          <FontAwesomeIcon icon={faEnvelope} />
+        </NavLink>
+      </nav>
+      <ul className='absolute bottom-6 m-6 '>
+        <li>
+          <a
+            href="https://linkedin.com/in/augustine-ogonna-odoh-627b44107"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FaLinkedinIn />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://github.com/austynomilan"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FaGithub />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://twitter.com/Austynomilan"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FaTwitter />
+          </a>
+        </li>
+      </ul>
     </div>
   )
 }
